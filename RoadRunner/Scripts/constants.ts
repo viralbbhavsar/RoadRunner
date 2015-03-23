3f92583ae7865cfd607fb40b42205dfd00e05ca2
viralbbhavsar <viralbbhavsar@gmail.com>
2015-03-22 20:25:43 -0400

    CreateJS Librarys added

---
+++ RoadRunner/Scripts/constants.ts
@@ -0,0 +1,21 @@
+﻿module constants {
+    export var FONT_SIZE: string = "40px";
+    export var FONT_FAMILY: string = "Consolas";
+    export var FONT_COLOUR: string = "#FFFF00";
+
+    export var SCREEN_WIDTH: number = 640;
+    export var SCREEN_HEIGHT: number = 480;
+    export var SCREEN_CENTER_WIDTH: number = SCREEN_WIDTH * 0.5;
+    export var SCREEN_CENTER_HEIGHT: number = SCREEN_HEIGHT * 0.5;
+    export var SPACE_RESET_WIDTH: number = 480;
+
+    export var PLAYER_LIVES: number = 5;
+    export var ENEMYCAR_NUM: number = 3;
+    export var FUEL_NUM: number = 2;
+
+    // States Constants
+    export var MENU_STATE: number = 0;
+    export var PLAY_STATE: number = 1;
+    export var GAME_OVER_STATE: number = 2;
+
+}    
